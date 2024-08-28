d2cd9f7f8285d4ebd61fae6abde943e30aa861cc
snow-jun-0 <wjo8703@naver.com>
2024-08-28 16:49:38 +0900

    Update and rename 2163.py to 1789-수들의 합.py

---
+++ BaekJoon/1789-수들의 합.py
@@ -0,0 +1,6 @@
+S=int(input())
+i=0
+while S>=0:
+    i+=1
+    S-=i
+print(i-1)
--- BaekJoon/2163.py
+++
@@ -1,6 +0,0 @@
-# ex2)
-# a, b, c = map(int, input().split())  // 
-# print((a+b)%c, ((a%c) + (b%c))%c, (a*b)%c, ((a%c) * (b%c))%c, sep='\n')
-
-n,m=map(int,input().split())
-print(n*m-1)
